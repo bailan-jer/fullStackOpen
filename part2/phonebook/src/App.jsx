3e6b6a043506de97e97ee232f039b61ee14b70a5
soonHong628 <soonhong2007@gmail.com>
2026-04-08 22:42:55 +0800

    Exercise 2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect} from 'react'
+import axios from "axios"
 
 const Filter = ({searchName, handleSearchName}) => {
   return (
@@ -31,12 +32,7 @@ const Persons = ({persons, searchName}) => {
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newPhone, setNewPhone] = useState("")
   const [searchName, setSearchName] = useState("")
@@ -62,7 +58,16 @@ const App = () => {
     setSearchName(event.target.value)
   }
 
-  const personsToShow = persons.filter(person => person.name.toLowerCase().includes(searchName.toLowerCase()))
+  useEffect(() => {
+    console.log("Effect")
+    axios
+      .get("http://localhost:3001/persons")
+      .then(response => {
+        console.log("promise fulfilled")
+        setPersons(response.data)
+      })
+  }, [])
+  console.log("render", persons.length, " persons")
 
   return (
     <div>
