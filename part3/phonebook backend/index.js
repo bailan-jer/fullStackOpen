bfd1320e189e0f89c2e6eee5a9640581505c96f6
bailan-jer <soonhong2007@gmail.com>
2026-08-18 22:05:27 +0800

    Exercise 3.3

--- part3/phonebook backend/index.js
+++ part3/phonebook backend/index.js
@@ -35,6 +35,16 @@ app.get("/info", (request, response) => {
     response.send(`<p>Phonebook has info for ${persons.length} people</p><p>${time}</p>`)
 })
 
+app.get("/api/persons/:id", (request, response) => {
+    const id = request.params.id
+    const person = persons.find(person => person.id === id)
+    if (!person){
+        return response.status(404).end()
+    }else{
+        return response.json(person)
+    }
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
