784786261c3fa408298999c49e0e5c4ce28e07fc
bailan-jer <soonhong2007@gmail.com>
2026-09-01 12:57:19 +0800

    Exercise 3.16

--- part3/phonebook backend/index.js
+++ part3/phonebook backend/index.js
@@ -44,8 +44,10 @@ let persons = [
     }
 ]
 
-app.get("/api/persons", (request, response) => {
-    Person.find({}).then(persons => response.json(persons))
+app.get("/api/persons", (request, response, next) => {
+    Person.find({})
+        .then(persons => response.json(persons))
+        .catch(error => next(error))
 })
 
 app.get("/info", (request, response) => {
@@ -55,12 +57,20 @@ app.get("/info", (request, response) => {
     response.send(`<p>Phonebook has info for ${persons.length} people</p><p>${time}</p>`)
 })
 
-app.get("/api/persons/:id", (request, response) => {
+app.get("/api/persons/:id", (request, response, next) => {
     const id = request.params.id
-    Person.findById(id).then(person => response.json(person))
+    Person.findById(id)
+        .then(person => {
+            if (person){
+                response.json(person)
+            } else {
+                response.status(404).end()
+            }
+        })
+        .catch(error => next(error))
 })
 
-app.delete("/api/persons/:id", (request, response) => {
+app.delete("/api/persons/:id", (request, response, next) => {
     const id = request.params.id
     Person.findByIdAndDelete(id)
         .then(deletedPerson => {
@@ -69,13 +79,10 @@ app.delete("/api/persons/:id", (request, response) => {
             }
             return response.status(204).end()
         })
-        .catch(error => {
-            console.log("Error: ", error.message)
-            response.status(400).end()
-        })
+        .catch(error => next(error))
 })
 
-app.post("/api/persons", (request, response) => {
+app.post("/api/persons", (request, response, next) => {
     const body = request.body
     if (!body.name.trim() || !body.number.trim()){
         return response.status(400).json({error: `name or number is empty`})
@@ -84,14 +91,25 @@ app.post("/api/persons", (request, response) => {
         name: body.name,
         number: body.number
     })
-    person.save().then(savedPerson => {
-        response.json(savedPerson)
-    })
+    person.save()
+        .then(savedPerson => {
+            response.json(savedPerson)
+        })
+        .catch(error => next(error))
 })
 
 app.use((request, response) => {
     response.status(404).send({error: "Invalid URL"})
 })
+
+const errorHandler = (error, request, response, next) => {
+    console.log("Error:", error.message)
+    if (error.name === "CastError"){
+        return response.status(400).json({"error": "malformed id"})
+    }
+    next(error)
+}
+app.use(errorHandler)
 
 const PORT = 3001
 app.listen(PORT, () => {
